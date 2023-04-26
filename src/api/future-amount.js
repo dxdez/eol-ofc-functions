99a0c709ef22af24d77c1c56b30cf82a4d69c668
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-04-25 22:47:26 -0400

    Add Lump Sum Functionality to Future Amount

--- src/api/future-amount.js
+++ src/api/future-amount.js
@@ -33,5 +33,44 @@ export default {
 		let duration = _duration;
 		let finalValue = Calculations.calculatePayment(rate, duration, 0, -_futureAmountRequired, DueDate.BegOfPeriod);
 		return Helper.getReturnValue(finalValue);
-	}
+	},
+	getFutureAmountLumpSumInvestment: function (returnAmount, amountRequired, durationYears, durationMonths) {
+		let _futureAmountRequired = Helper.getNumber(amountRequired);
+		let _durYears = Helper.getNumber(durationYears);
+		let _durMonths = Helper.getNumber(durationMonths);
+		let calculatedReturnPercentage = Helper.getNumber(returnAmount / 100);
+		let calculatedTime = _durYears + (_durMonths / 12);
+		let finalValue = _futureAmountRequired / Math.pow((1 + calculatedReturnPercentage), calculatedTime);
+		return Helper.getReturnValue(finalValue);
+	},
+	getFutureAmountPeriodicLumpSumInvestment: function(returnAmount, amountRequired, amountToday, durationYears, durationMonths, frequency) {
+		let _futureAmountRequired = Helper.getNumber(amountRequired);
+		let _amountToday = Helper.getNumber(amountToday);
+		let _durYears = Helper.getNumber(durationYears);
+		let _durMonths = Helper.getNumber(durationMonths);
+		let _frequencySelection = frequency;
+		let _frequency = 0;
+		let _duration = 0;
+		switch (_frequencySelection)
+		{
+			case Tags.frequencyAnnual:
+				_frequency = 1;
+				_duration = _durYears + (_durMonths / 12);
+				break;
+			case Tags.frequencyQuarterly:
+				_frequency = 4;
+				_duration = (_durYears * 4) + (_durMonths / 3);
+				break;
+			case Tags.frequencyMonthly:
+				_frequency = 12;
+				_duration = (_durYears * 12) + _durMonths;
+				break;
+			default:
+				_frequency = 0;
+				_duration = 0;
+				break;
+		}
+		let finalValue = Calculations.calculatePayment(Math.pow((1.0 + (returnAmount / 100.0)), (1.0 / _frequency)) - 1, _duration, _amountToday, -_futureAmountRequired, DueDate.BegOfPeriod);
+		return Helper.getReturnValue(finalValue);
+	},
 }
