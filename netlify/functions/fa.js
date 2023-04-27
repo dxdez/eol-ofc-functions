6ce17d5df8732b92e4f5b40c028cbc68bc57bd09
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-04-26 23:41:34 -0400

    Add Additional Endpoints for Future Amount

--- netlify/functions/fa.js
+++ netlify/functions/fa.js
@@ -1,11 +1,17 @@
 import FutureAmount from '../../src/api/future-amount.js';
 
 exports.handler = async (event, context) => {
-	const { mode, ra, ar, dy, dm, fr } = event.queryStringParameters;
+	const { mode, ra, ar, dy, dm, fr, at } = event.queryStringParameters;
 	let result;
 	switch(mode) {
 		case 'periodic-investment':
 			result = FutureAmount.getFutureAmountPeriodicInvestment(ra, ar, dy, dm, fr);	
+			break;
+		case 'lumpsum-investment':
+			result = FutureAmount.getFutureAmountLumpSumInvestment(ra, ar, dy, dm);
+			break;
+		case 'periodic-lumpsum-investment':
+			result = FutureAmount.getFutureAmountPeriodicLumpSumInvestment(ra, ar, at, dy, dm, fr);
 			break;
 		default:
 			result = 'Not Found'
