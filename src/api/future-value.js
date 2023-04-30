4ea97c7b187b3acabecc2f9f92e9262f91d00bca
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-04-29 22:13:56 -0400

    Add additional future value functions

--- src/api/future-value.js
+++ src/api/future-value.js
@@ -13,5 +13,24 @@ export default {
 		let newRate = Math.pow(newReturnAmount, (12.0 / _frequency)) - 1.0;
 		let finalValue = Calculations.calculateFutureValue(newRate, (_parameterYear * _frequency), -_investmentAmount, 0, DueDate.BegOfPeriod);
 		return Helper.getReturnValue(finalValue);
+	},
+	getFutureValueLumpSum_annualCompoundReturn: function (lumpSumInvested, year, percentage) {
+		let _lumpSumInvested = Helper.getNumber(lumpSumInvested);
+		let _percentage = Helper.getNumber(percentage);
+		let _parameterYear = Helper.getNumber(year);
+		let finalValue = _lumpSumInvested * Math.pow(1 + (_percentage / 100), _parameterYear);
+		return Helper.getReturnValue(finalValue);
+	},
+	getFutureValuePeriodicLumpSum_annualCompoundReturn: function (investmentAmount, lumpSumInvested, investmentFrequecy, year, percentage) {
+		let _investmentAmount = Helper.getNumber(investmentAmount);
+		let _lumpSumInvested = Helper.getNumber(lumpSumInvested);
+		let _percentage = Helper.getNumber(percentage);
+		let _frequency = Helper.getValueFromFrequencyV2(investmentFrequecy);
+		let _parameterYear = Helper.getNumber(year);
+		let per = (_percentage / 100.0);
+		let newReturnAmount = Math.pow((1.0 + per), (1.0 / 12.0));
+		let newRate = Math.pow(newReturnAmount, (12.0 / _frequency)) - 1.0;
+		let finalValue = Calculations.calculateFutureValue(newRate, (_parameterYear * _frequency), -_investmentAmount, -_lumpSumInvested, DueDate.BegOfPeriod);
+		return Helper.getReturnValue(finalValue);
 	}
 }
