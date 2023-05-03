db80422d06f8509f65da72b993ac843faf3ad857
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-05-02 22:40:26 -0400

    Add Lumpsum Endpoint for future value functionality

--- netlify/functions/fv.js
+++ netlify/functions/fv.js
@@ -1,12 +1,17 @@
 import FutureValue from '../../src/api/future-value.js';
 
 exports.handler = async (event, context) => {
-	const { mode, ia, if, y, p } = event.queryStringParameters;
+	const { mode, ia, li, if, y, p } = event.queryStringParameters;
 	let result;
 	switch(mode) {
 		case 'periodic-annual-compound-return':
 			result = FutureValue.getFutureValuePeriodic_annualCompoundReturn(ia, if, y, p);
+			breal
+		case 'lumpsum-annual-compound-return':
+			result = FutureValue.getFutureValueLumpSum_annualCompoundReturn(li, y, p);
 			break;
+		default:
+			result = 'Not Found';
 	}
 	return {
 		statusCode: 200,
