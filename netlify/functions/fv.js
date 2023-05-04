dd471306243b3c5ebdfcdbd4e9ef226c46725cf6
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-05-03 22:01:15 -0400

    Add additional future value function

--- netlify/functions/fv.js
+++ netlify/functions/fv.js
@@ -1,14 +1,17 @@
 import FutureValue from '../../src/api/future-value.js';
 
 exports.handler = async (event, context) => {
-	const { mode, ia, li, if, y, p } = event.queryStringParameters;
+	const { mode, ia, li, ifr, y, p } = event.queryStringParameters;
 	let result;
 	switch(mode) {
 		case 'periodic-annual-compound-return':
-			result = FutureValue.getFutureValuePeriodic_annualCompoundReturn(ia, if, y, p);
+			result = FutureValue.getFutureValuePeriodic_annualCompoundReturn(ia, ifr, y, p);
 			break;
 		case 'lumpsum-annual-compound-return':
 			result = FutureValue.getFutureValueLumpSum_annualCompoundReturn(li, y, p);
+			break;
+		case 'periodic-lumpsum-annual-compound-return':
+			result = FutureValue.getFutureValuePeriodicLumpSum_annualCompoundReturn(ia, li, ifr, y, p);
 			break;
 		default:
 			result = 'Not Found';
