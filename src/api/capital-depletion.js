ef8380d1ee1bc0eb49cd0a6dc1e79a777b097636
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-05-04 23:09:17 -0400

    Add Capital Depletion functions

---
+++ src/api/capital-depletion.js
@@ -0,0 +1,23 @@
+import Calculations from '../services/calculator.js';
+import Helper from '../services/helper.js';
+import { Tags, DueDate } from '../config/globals.js';
+
+export default {
+	capitalDepletion_getAmountPerWithdrawl: function (presentValue, years, withdrawls, frequency, returnPercentage) {
+		let _presentValue = Helper.getNumber(presentValue);
+		let _percentage = Helper.getNumber(returnPercentage);
+		let _parameterYear = Helper.getNumber(years);
+		let _numberOfWithdrawls = Helper.getNumber(withdrawls);
+		let _frequency = Helper.getValueFromFrequencyAlt(frequency);
+		let returnAmount = (Math.pow((1 + (_percentage / 100) * (1.0 / _frequency)), _frequency)) - 1;
+		let finalValue = -Calculations.calculatePayment(Math.pow((1 + returnAmount), (1 / _numberOfWithdrawls)) - 1, (_numberOfWithdrawls * _parameterYear), _presentValue, 0, DueDate.EndOfPeriod);
+		return Helper.getReturnValue(finalValue);
+	},
+	capitalDepletion_getTotalPayout: function (amountPerWithdrawl, years, withdrawls) {
+		let _amount = Helper.getNumber(amountPerWithdrawl);
+		let _parameterYear = Helper.getNumber(years);
+		let _numberOfWithdrawls = Helper.getNumber(withdrawls);
+		let finalValue = _amount * _parameterYear * _numberOfWithdrawls;
+		return Helper.getReturnValue(finalValue);
+	}
+}
--- src/services/helper.js
+++ src/services/helper.js
@@ -1,37 +1,51 @@
 import { Tags } from '../config/globals.js';
 
 export default {
-    getReturnValue: function (checkValue) {
-        if (isNaN(checkValue) || isNull(checkValue) || !isFinite(checkValue)) {
-            return "-";
-        } else {
-            return checkValue.toFixed(2);
-        }
-    },
-    getValueFromFrequency: function (selection) {
-        switch(selection) {
-            case Tags.frequencyAnnual:
-                return 12; // 12 months in a year
-            case Tags.frequencyQuarterly:
-                return 3; // 3 months in a quarter of a year
-            case Tags.frequencyMonthly:
-                return 1; // 1 month representing a single month
-            default:
-                return 0; // Misc values return 0 by default
-        }
-    },
-    getNumber: function (returnValue, returnStrict = false) {
-        if (returnStrict) {
-            return parseInt(returnValue) || 0;
-        } else {
-            return parseFloat(returnValue) || 0;
-        }
-    },
-    getReturnValue: function (checkValue) {
-        if (isNaN(checkValue) || checkValue == null || !isFinite(checkValue)) { // Is Nan, Null or Infinity
-            return "-";
-        } else {
-            return checkValue.toFixed(2); // Convert value to display as 00.00 decimal
-        }
-    }
+	getReturnValue: function (checkValue) {
+		if (isNaN(checkValue) || isNull(checkValue) || !isFinite(checkValue)) {
+			return "-";
+		} else {
+			return checkValue.toFixed(2);
+		}
+	},
+	getValueFromFrequency: function (selection) {
+		switch(selection) {
+			case Tags.frequencyAnnual:
+				return 12; // 12 months in a year
+			case Tags.frequencyQuarterly:
+				return 3; // 3 months in a quarter of a year
+			case Tags.frequencyMonthly:
+				return 1; // 1 month representing a single month
+			default:
+				return 0; // Misc values return 0 by default
+		}
+	},
+	getValueFromFrequencyAlt: function (selection) {
+		switch(selection) {
+			case Tags.frequencyAnnual:
+				return 1;
+			case Tags.frequencySemiAnnual:
+				return 2;
+			case Tags.frequencyQuarterly:
+				return 4;                    
+			case Tags.frequencyMonthly:
+				return 12;            
+			default:
+				return 0; // Misc values return 0 by default
+		}
+	},
+	getNumber: function (returnValue, returnStrict = false) {
+		if (returnStrict) {
+			return parseInt(returnValue) || 0;
+		} else {
+			return parseFloat(returnValue) || 0;
+		}
+	},
+	getReturnValue: function (checkValue) {
+		if (isNaN(checkValue) || checkValue == null || !isFinite(checkValue)) { // Is Nan, Null or Infinity
+			return "-";
+		} else {
+			return checkValue.toFixed(2); // Convert value to display as 00.00 decimal
+		}
+	}
 }
