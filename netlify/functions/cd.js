2215ab35ab5502c0709bcd40b320cf652b91f020
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-05-06 21:55:17 -0400

    Add total payout endpoint

--- netlify/functions/cd.js
+++ netlify/functions/cd.js
@@ -1,11 +1,14 @@
 import CapitalDepletion from '../../src/api/capital-depletion.js';
 
 exports.handler = async (event, context) => {
-	const { mode, pv, y, w, f, rp } = event.queryStringParameters;
+	const { mode, pv, y, w, f, rp, apw } = event.queryStringParameters;
 	let result;
 	switch(mode) {
 		case 'get-amount-per-withdrawl':
 			result = CapitalDepletion.capitalDepletion_getAmountPerWithdrawl(pv, y, w, f, rp);
+			break;
+		case 'get-total-payout':
+			result = CapitalDepletion.getTotalPayout(apw, y, w);
 			break;
 		default:
 			result = 'Not Found';
