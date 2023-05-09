643f1dfec43d281b2c1f12ed1fe1d2ad2791290f
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-05-08 22:38:44 -0400

    Add support functions for major purchase feature

--- src/api/major-purchase.js
+++ src/api/major-purchase.js
@@ -17,5 +17,20 @@ export default {
 		let calculatedCompound = _annualCompoundReturn / 100;
 		let finalValue = calculatedReturnCapital / Math.pow(1 + calculatedCompound, _numberOfYears);
 		return Helper.getReturnValue(finalValue);
+	},
+	majorPurchase_getLumpSumRequired: function (lumpSum, existingCapital) {
+		let finalValue = lumpSum - existingCapital;
+		return Helper.getReturnValue(finalValue);
+	},
+	majorPurchase_getMonthlyInvestments: function (annualCompoundReturn, numberYears, capitalAccumulated, calculatedReturnCapital) {
+		let _capitalAccumulated = Helper.getNumber(capitalAccumulated);
+		let _annualCompoundReturn = Helper.getNumber(annualCompoundReturn);
+		let _numberOfYears = Helper.getNumber(numberYears);
+		let calculatedAnnualCompoundReturn = 1 + (_annualCompoundReturn / 100);
+		let calculatedTime = _numberOfYears * 12;
+		let calculatedRate = Math.pow(calculatedAnnualCompoundReturn, 1.0 / 12) - 1;
+		let calculatedValue = calculatedReturnCapital - (_capitalAccumulated * Math.pow(calculatedAnnualCompoundReturn, _numberOfYears));
+		let finalValue = -Calculations.calculatePayment(calculatedRate, calculatedTime, 0, calculatedValue, DueDate.BegOfPeriod);
+		return Helper.getReturnValue(finalValue);
 	}
 }
