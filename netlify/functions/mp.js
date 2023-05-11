adb1396fc500af63901cdda1287928a62ddc3f58
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-05-10 21:54:01 -0400

    Add new endpoints for major purchase

--- netlify/functions/mp.js
+++ netlify/functions/mp.js
@@ -1,11 +1,17 @@
 import MajorPurchase from '../../src/api/major-purchase.js';
 
 exports.handler = async (event, context) => {
-	const { mode, cr, ny, ai } = event.queryStringParameters;
+	const { mode, cr, ny, ai, acr, crc, ls, ec } = event.queryStringParameters;
 	let result;
 	switch(mode) {
 		case 'get-capital-required':
 			result = MajorPurchase.getCapitalRequired(cr, ny, ai);
+			break;
+		case 'get-lumpsum-today':
+			result = MajorPurchase.getLumpSumToday(acr, ny, crc);
+			break;
+		case 'get-lumpsum-required':
+			result = MajorPurchase.getLumpSumRequired(ls, ec); 
 			break;
 		default:
 			result = 'Not Found';
