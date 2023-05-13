8891b556b35e7ed460d5293bb5f30d8fe9d8b0ae
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-05-12 22:43:55 -0400

    Add withdrawl data function for desired income

--- src/api/desired-income.js
+++ src/api/desired-income.js
@@ -37,5 +37,52 @@ export default {
 			finalValue = Calculations.presentValue(presentValueRate, _parameterWithdrawlFrequency, -_parameterIncomeDesired, -currentCapitalRemaining, DueDate.EndOfPeriod);        
 		}
 		return Helper.getReturnValue(finalValue);
+	},
+	desiredIncome_getWithdrawlData: function(periodicIncomeDesired, annualInflationRate, withdrawlFrequency, durationYears, capitalReturnCompounding, compoundingFrequency) {
+		let _parameterIncomeDesired = Helper.getNumber(periodicIncomeDesired);
+		let _parameterAnnualInflationRate = Helper.getNumber(annualInflationRate);
+		let _parameterCapitalReturnCompounding = Helper.getNumber(capitalReturnCompounding);
+		let _parameterDurationYears = Helper.getNumber(durationYears, true);
+		let _parameterWithdrawlFrequency = Helper.getValueFromFrequencyAlt(withdrawlFrequency);
+		let _parameterCompoundingFrequency = Helper.getValueFromFrequencyAlt(compoundingFrequency);
+		if (_parameterWithdrawlFrequency > 0 && _parameterDurationYears > 0) {
+			let calculatedIndex = _parameterAnnualInflationRate / 100;
+			let compoundIndex = _parameterCapitalReturnCompounding / 100;
+			let returnRate = Math.pow((1.0 + (compoundIndex * (1.0 / _parameterCompoundingFrequency))), (_parameterCompoundingFrequency / 1.0)) - 1;
+			let presentValueRate = Math.pow(1.0 + returnRate, 1.0 / _parameterWithdrawlFrequency) - 1;
+
+			let currentIncomeDesiredValues = [];
+			let capitalRemainingValues = [];
+			let mainData = [];
+
+			let currentIncomeDesired = _parameterIncomeDesired;
+			for (let i = 0; i < _parameterDurationYears; i++) {
+				currentIncomeDesiredValues.push(currentIncomeDesired);
+				currentIncomeDesired *= (1 + calculatedIndex);
+			}
+			let nextWithdrawl = 0;
+			let currentCapitalRemaining = 0;
+			for (let j = _parameterDurationYears; j > 0; j--) {
+				let capitalRemaining = 0;
+				if (j < _parameterDurationYears) {
+					nextWithdrawl = currentIncomeDesiredValues[j];
+					capitalRemaining = Calculations.presentValue(presentValueRate, _parameterWithdrawlFrequency, -nextWithdrawl, -currentCapitalRemaining, DueDate.EndOfPeriod);
+				}
+				capitalRemainingValues.push(capitalRemaining);
+				currentCapitalRemaining = capitalRemaining;
+			}
+			capitalRemainingValues.reverse();
+			for(let k = 0; k < currentIncomeDesiredValues.length; k++) {
+				let currentIndexyear = (k + 1);
+				mainData.push({
+					id: `000${currentIndexyear}`,
+					year: currentIndexyear,
+					withdrawls: currentIncomeDesiredValues[k],
+					remainingValue: capitalRemainingValues[k]
+				});
+			}
+			return mainData;
+		}
+		return [];
 	}
 }
