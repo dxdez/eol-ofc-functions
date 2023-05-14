6052d4823588541b72033c55836a0980c57a1876
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-05-13 22:06:44 -0400

    Add syntax fix for future value functionality

--- src/api/future-value.js
+++ src/api/future-value.js
@@ -6,7 +6,7 @@ export default {
 	getFutureValuePeriodic_annualCompoundReturn: function (investmentAmount, investmentFrequecy, year, percentage) {
 		let _investmentAmount = Helper.getNumber(investmentAmount);
 		let _percentage = Helper.getNumber(percentage);
-		let _frequency = Helper.getValueFromFrequencyV2(investmentFrequecy);
+		let _frequency = Helper.getValueFromFrequencyAlt(investmentFrequecy);
 		let _parameterYear = Helper.getNumber(year);
 		let per = (_percentage / 100.0);
 		let newReturnAmount = Math.pow((1.0 + per), (1.0 / 12.0));
@@ -25,7 +25,7 @@ export default {
 		let _investmentAmount = Helper.getNumber(investmentAmount);
 		let _lumpSumInvested = Helper.getNumber(lumpSumInvested);
 		let _percentage = Helper.getNumber(percentage);
-		let _frequency = Helper.getValueFromFrequencyV2(investmentFrequecy);
+		let _frequency = Helper.getValueFromFrequencyAlt(investmentFrequecy);
 		let _parameterYear = Helper.getNumber(year);
 		let per = (_percentage / 100.0);
 		let newReturnAmount = Math.pow((1.0 + per), (1.0 / 12.0));
