ac0c52bfb5ce718d27d07d247c19a44fb55e9a06
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-05-15 21:14:06 -0400

    Update README and Rate of Return comments

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ofc-serverless-functions
-An api that uses netlify serverless functions to return financial calculations
+
+The Open Financial Calculator is an application that provides basic financial calculations to assist users with financial planning. The application utilizes serverless functions on Netlify, and the provided functions are used to calculate rates of return for various financial scenarios, such as annual compound return, total percentage gain, and lump sum average annual gain. In addition to the rate of return functionality, the Open Financial Calculator application also provides several other financial calculations to assist users with their financial planning. Some of these additional functions include:
+
+Future Value: Calculates the future value of an investment based on a specified interest rate and compounding period, as well as the initial investment amount and the length of the investment.
+
+Future Amount: Calculates the future amount of a regular investment based on a specified interest rate and compounding period, as well as the initial investment amount, the length of the investment, and the regular investment amount.
+
+Major Purchase: Calculates the required amount of money that needs to be saved on a regular basis in order to reach a specified financial goal, such as a down payment on a home or a college education.
+
+Capital Depletion: Tracks the depletion of capital over time based on a specified investment rate of return, initial investment amount, and regular withdrawals.
+
+Retirement Income: Calculates the amount of money that needs to be saved on a regular basis in order to achieve a desired level of income in retirement, based on a specified interest rate and the length of the retirement period.
+
+All of these calculations can be very useful in helping users make informed financial decisions and plan for their future financial goals.
--- src/api/rate-of-return.js
+++ src/api/rate-of-return.js
@@ -3,6 +3,7 @@ import Helper from '../services/helper.js';
 import { DueDate } from '../config/globals.js';
 
 export default {
+	// This function calculates the annual compound rate of return on an investment that has been made over a period of time, given the amount invested, the present value of the investment, the duration of the investment (in years and months), and the frequency of compounding. 
 	getRateOfReturnAnnualCompoundReturn: function (amountValue, presentValue, durationYears, durationMonths, frequency) {
 		let _amount = Helper.getNumber(amountValue);
 		let _presentVal = Helper.getNumber(presentValue);
@@ -14,6 +15,7 @@ export default {
 		let finalValue = (Math.pow((1 + rate), (12 / _freq)) - 1) * 100;
 		return Helper.getReturnValue(finalValue);
 	},
+	// This function calculates the total percentage gain or loss on an investment that has been made over a period of time, given the amount invested, the present value of the investment, the duration of the investment (in years and months), and the frequency of compounding. 
 	getRateOfReturnTotalPercentageGain: function (amountValue, presentValue, durationYears, durationMonths, frequency) {
 		let _amount = Helper.getNumber(amountValue);
 		let _presentVal = Helper.getNumber(presentValue);
@@ -24,6 +26,7 @@ export default {
 		let finalValue = 100 * (_presentVal - (_amount * periods)) / (_amount * periods);
 		return Helper.getReturnValue(finalValue);
 	},
+	// This function calculates the annual compound rate of return on an investment that was made as a lump sum, given the amount invested, the present value of the investment, and the duration of the investment (in years and months). 
 	getRateOfReturnLumpSumAnnualCompoundReturn: function(lumpInvested, presentValue, durationYears, durationMonths) {
 		let _investment = Helper.getNumber(lumpInvested);
 		let _presentVal = Helper.getNumber(presentValue);
@@ -33,12 +36,14 @@ export default {
 		let finalValue = (Math.pow((_presentVal / _investment), (1 / _time)) - 1) * 100;
 		return Helper.getReturnValue(finalValue);
 	},
+	// This function calculates the total percentage gain or loss on an investment that was made as a lump sum, given the amount invested and the present value of the investment. 
 	getRateOfReturnLumpSumTotalPercentageGain: function (lumpInvested, presentValue) {
 		let _investment = Helper.getNumber(lumpInvested);
 		let _presentVal = Helper.getNumber(presentValue);
 		let finalValue = 100 * (_presentVal - _investment) / _investment;
 		return Helper.getReturnValue(finalValue);
 	},
+	// This function calculates the average annual rate of return on an investment that was made as a lump sum, given the amount invested, the present value of the investment, and the duration of the investment (in years and months). 
 	getRateOfReturnLumpSumAverageAnnualGain: function (lumpInvested, presentValue, durationYears, durationMonths) {
 		let _investment = Helper.getNumber(lumpInvested);
 		let _presentVal = Helper.getNumber(presentValue);
