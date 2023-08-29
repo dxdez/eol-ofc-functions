5cb9a6c9c46bf536a73ddc9da4c165c4f0779243
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-08-29 00:03:43 -0400

    refactor: cleanup formatting and var names for cd, di and fa classes

--- src/api/desired-income.js
+++ src/api/desired-income.js
@@ -1,89 +1,94 @@
 import Calculations from '../services/calculator.js';
 import Helper from '../services/helper.js';
-import { Tags, DueDate } from '../config/globals.js';
+import {
+  Tags,
+  DueDate
+} from '../config/globals.js';
 
 export default {
-	totalIncomeRequired: function(periodicIncomeDesired, annualInflationRate, withdrawlFrequency, durationYears, capitalReturnCompounding, compoundingFrequency) {
-		let _parameterIncomeDesired = Helper.getNumber(periodicIncomeDesired);
-		let _parameterAnnualInflationRate = Helper.getNumber(annualInflationRate);
-		let _parameterCapitalReturnCompounding = Helper.getNumber(capitalReturnCompounding);
-		let _parameterDurationYears = Helper.getNumber(durationYears, true);
-		let _parameterWithdrawlFrequency = Helper.getValueFromFrequencyAlt(withdrawlFrequency);
-		let _parameterCompoundingFrequency = Helper.getValueFromFrequencyAlt(compoundingFrequency);
-		let finalValue = NaN;
-		if (_parameterWithdrawlFrequency > 0 && _parameterDurationYears > 0) {
-			let calculatedIndex = _parameterAnnualInflationRate / 100;
-			let compoundIndex = _parameterCapitalReturnCompounding / 100;
-			let returnRate = Math.pow((1.0 + (compoundIndex * (1.0 / _parameterCompoundingFrequency))), (_parameterCompoundingFrequency / 1.0)) - 1;
-			let presentValueRate = Math.pow(1.0 + returnRate, 1.0 / _parameterWithdrawlFrequency) - 1;
+  totalIncomeRequired: function (periodicIncomeDesired, annualInflationRate, withdrawlFrequency, durationYears, capitalReturnCompounding, compoundingFrequency) {
+    let valueIncomeDesired = Helper.getNumber(periodicIncomeDesired);
+    let valueAnnualInflationRate = Helper.getNumber(annualInflationRate);
+    let valueCapitalReturnCompounding = Helper.getNumber(capitalReturnCompounding);
+    let valueDurationYears = Helper.getNumber(durationYears, true);
+    let valueWithdrawlFrequency = Helper.getValueFromFrequencyAlt(withdrawlFrequency);
+    let valueCompoundingFrequency = Helper.getValueFromFrequencyAlt(compoundingFrequency);
+    let finalValue = NaN;
+    if (valueWithdrawlFrequency > 0 && valueDurationYears > 0) {
+      let calculatedIndex = valueAnnualInflationRate / 100;
+      let compoundIndex = valueCapitalReturnCompounding / 100;
+      let returnRate = Math.pow((1.0 + (compoundIndex * (1.0 / valueCompoundingFrequency))), (valueCompoundingFrequency / 1.0)) - 1;
+      let presentValueRate = Math.pow(1.0 + returnRate, 1.0 / valueWithdrawlFrequency) - 1;
 
-			let currentIncomeDesiredValues = [];
+      let currentIncomeDesiredValues = [];
 
-			let currentIncomeDesired = _parameterIncomeDesired;
-			for (let i = 0; i < _parameterDurationYears; i++) {
-				currentIncomeDesiredValues.push(currentIncomeDesired);
-				currentIncomeDesired *= (1 + calculatedIndex);
-			}
-			let nextWithdrawl = 0;
-			let currentCapitalRemaining = 0;   
-			for (let j = _parameterDurationYears; j > 0; j--) {
-				let capitalRemaining = 0;
-				if (j < _parameterDurationYears) {
-					nextWithdrawl = currentIncomeDesiredValues[j];
-					capitalRemaining = Calculations.calculatePresentValue(presentValueRate, _parameterWithdrawlFrequency, -nextWithdrawl, -currentCapitalRemaining, DueDate.EndOfPeriod);
-				}
-				currentCapitalRemaining = capitalRemaining;
-			}
-			finalValue = Calculations.calculatePresentValue(presentValueRate, _parameterWithdrawlFrequency, -_parameterIncomeDesired, -currentCapitalRemaining, DueDate.EndOfPeriod);        
-		}
-		return Helper.getReturnValue(finalValue);
-	},
-	getWithdrawlData: function(periodicIncomeDesired, annualInflationRate, withdrawlFrequency, durationYears, capitalReturnCompounding, compoundingFrequency) {
-		let _parameterIncomeDesired = Helper.getNumber(periodicIncomeDesired);
-		let _parameterAnnualInflationRate = Helper.getNumber(annualInflationRate);
-		let _parameterCapitalReturnCompounding = Helper.getNumber(capitalReturnCompounding);
-		let _parameterDurationYears = Helper.getNumber(durationYears, true);
-		let _parameterWithdrawlFrequency = Helper.getValueFromFrequencyAlt(withdrawlFrequency);
-		let _parameterCompoundingFrequency = Helper.getValueFromFrequencyAlt(compoundingFrequency);
-		let withdrawlData = { wd: [] };
-		if (_parameterWithdrawlFrequency > 0 && _parameterDurationYears > 0) {
-			let calculatedIndex = _parameterAnnualInflationRate / 100;
-			let compoundIndex = _parameterCapitalReturnCompounding / 100;
-			let returnRate = Math.pow((1.0 + (compoundIndex * (1.0 / _parameterCompoundingFrequency))), (_parameterCompoundingFrequency / 1.0)) - 1;
-			let presentValueRate = Math.pow(1.0 + returnRate, 1.0 / _parameterWithdrawlFrequency) - 1;
+      let currentIncomeDesired = valueIncomeDesired;
+      for (let i = 0; i < valueDurationYears; i++) {
+        currentIncomeDesiredValues.push(currentIncomeDesired);
+        currentIncomeDesired *= (1 + calculatedIndex);
+      }
+      let nextWithdrawl = 0;
+      let currentCapitalRemaining = 0;
+      for (let j = valueDurationYears; j > 0; j--) {
+        let capitalRemaining = 0;
+        if (j < valueDurationYears) {
+          nextWithdrawl = currentIncomeDesiredValues[j];
+          capitalRemaining = Calculations.calculatePresentValue(presentValueRate, valueWithdrawlFrequency, -nextWithdrawl, -currentCapitalRemaining, DueDate.EndOfPeriod);
+        }
+        currentCapitalRemaining = capitalRemaining;
+      }
+      finalValue = Calculations.calculatePresentValue(presentValueRate, valueWithdrawlFrequency, -valueIncomeDesired, -currentCapitalRemaining, DueDate.EndOfPeriod);
+    }
+    return Helper.getReturnValue(finalValue);
+  },
+  getWithdrawlData: function (periodicIncomeDesired, annualInflationRate, withdrawlFrequency, durationYears, capitalReturnCompounding, compoundingFrequency) {
+    let valueIncomeDesired = Helper.getNumber(periodicIncomeDesired);
+    let valueAnnualInflationRate = Helper.getNumber(annualInflationRate);
+    let valueCapitalReturnCompounding = Helper.getNumber(capitalReturnCompounding);
+    let valueDurationYears = Helper.getNumber(durationYears, true);
+    let valueWithdrawlFrequency = Helper.getValueFromFrequencyAlt(withdrawlFrequency);
+    let valueCompoundingFrequency = Helper.getValueFromFrequencyAlt(compoundingFrequency);
+    let withdrawlData = {
+      wd: []
+    };
+    if (valueWithdrawlFrequency > 0 && valueDurationYears > 0) {
+      let calculatedIndex = valueAnnualInflationRate / 100;
+      let compoundIndex = valueCapitalReturnCompounding / 100;
+      let returnRate = Math.pow((1.0 + (compoundIndex * (1.0 / valueCompoundingFrequency))), (valueCompoundingFrequency / 1.0)) - 1;
+      let presentValueRate = Math.pow(1.0 + returnRate, 1.0 / valueWithdrawlFrequency) - 1;
 
-			let currentIncomeDesiredValues = [];
-			let capitalRemainingValues = [];
-			let mainData = [];
+      let currentIncomeDesiredValues = [];
+      let capitalRemainingValues = [];
+      let mainData = [];
 
-			let currentIncomeDesired = _parameterIncomeDesired;
-			for (let i = 0; i < _parameterDurationYears; i++) {
-				currentIncomeDesiredValues.push(currentIncomeDesired);
-				currentIncomeDesired *= (1 + calculatedIndex);
-			}
-			let nextWithdrawl = 0;
-			let currentCapitalRemaining = 0;
-			for (let j = _parameterDurationYears; j > 0; j--) {
-				let capitalRemaining = 0;
-				if (j < _parameterDurationYears) {
-					nextWithdrawl = currentIncomeDesiredValues[j];
-					capitalRemaining = Calculations.calculatePresentValue(presentValueRate, _parameterWithdrawlFrequency, -nextWithdrawl, -currentCapitalRemaining, DueDate.EndOfPeriod);
-				}
-				capitalRemainingValues.push(capitalRemaining);
-				currentCapitalRemaining = capitalRemaining;
-			}
-			capitalRemainingValues.reverse();
-			for(let k = 0; k < currentIncomeDesiredValues.length; k++) {
-				let currentIndexyear = (k + 1);
-				mainData.push({
-					id: `000${currentIndexyear}`,
-					year: currentIndexyear,
-					withdrawls: currentIncomeDesiredValues[k],
-					remainingValue: capitalRemainingValues[k]
-				});
-			}
-			withdrawlData.wd = mainData;
-		}
-		return JSON.stringify(withdrawlData);
-	}
+      let currentIncomeDesired = valueIncomeDesired;
+      for (let i = 0; i < valueDurationYears; i++) {
+        currentIncomeDesiredValues.push(currentIncomeDesired);
+        currentIncomeDesired *= (1 + calculatedIndex);
+      }
+      let nextWithdrawl = 0;
+      let currentCapitalRemaining = 0;
+      for (let j = valueDurationYears; j > 0; j--) {
+        let capitalRemaining = 0;
+        if (j < valueDurationYears) {
+          nextWithdrawl = currentIncomeDesiredValues[j];
+          capitalRemaining = Calculations.calculatePresentValue(presentValueRate, valueWithdrawlFrequency, -nextWithdrawl, -currentCapitalRemaining, DueDate.EndOfPeriod);
+        }
+        capitalRemainingValues.push(capitalRemaining);
+        currentCapitalRemaining = capitalRemaining;
+      }
+      capitalRemainingValues.reverse();
+      for (let k = 0; k < currentIncomeDesiredValues.length; k++) {
+        let currentIndexyear = (k + 1);
+        mainData.push({
+          id: `000${currentIndexyear}`,
+          year: currentIndexyear,
+          withdrawls: currentIncomeDesiredValues[k],
+          remainingValue: capitalRemainingValues[k]
+        });
+      }
+      withdrawlData.wd = mainData;
+    }
+    return JSON.stringify(withdrawlData);
+  }
 }
--- src/api/future-amount.js
+++ src/api/future-amount.js
@@ -1,115 +1,116 @@
 import Calculations from '../services/calculator.js';
 import Helper from '../services/helper.js';
-import { Tags, DueDate } from '../config/globals.js';
+import {
+  Tags,
+  DueDate
+} from '../config/globals.js';
 
 export default {
-	// Calculates the future value of a periodic investment. The function takes the return rate, the required amount, the duration in years and months, and the frequency of investment as inputs. It returns the future value of the investment.
-	getFutureAmountPeriodicInvestment: function (returnAmount, amountRequired, durationYears, durationMonths, frequency) {
-		let _futureAmountRequired = Helper.getNumber(amountRequired);
-		let _durYears = Helper.getNumber(durationYears);
-		let _durMonths = Helper.getNumber(durationMonths);
-		let _frequencySelection = frequencytoUpperCase();
-		let _frequency = 0;
-		let _duration = 0;
-		switch (_frequencySelection)
-		{
-			case Tags.frequencyAnnual:
-				_frequency = 1;
-				_duration = _durYears + (_durMonths / 12);
-				break;
-			case Tags.frequencyQuarterly:
-				_frequency = 4;
-				_duration = (_durYears * 4) + (_durMonths / 3);
-				break;
-			case Tags.frequencyMonthly:
-				_frequency = 12;
-				_duration = (_durYears * 12) + _durMonths;
-				break;
-			default:
-				_frequency = 0;
-				_duration = 0;
-				break;
-		}
-		let rate = Math.pow((1.0 + (returnAmount / 100)), (1.0 / _frequency)) - 1;
-		let duration = _duration;
-		let finalValue = Calculations.calculatePayment(rate, duration, 0, -_futureAmountRequired, DueDate.BegOfPeriod);
-		return Helper.getReturnValue(finalValue);
-	},
-	// Calculates the future value of a lump sum investment. The function takes the return rate, the required amount, and the duration in years and months as inputs. It returns the future value of the investment.
-	getFutureAmountLumpSumInvestment: function (returnAmount, amountRequired, durationYears, durationMonths) {
-		let _futureAmountRequired = Helper.getNumber(amountRequired);
-		let _durYears = Helper.getNumber(durationYears);
-		let _durMonths = Helper.getNumber(durationMonths);
-		let calculatedReturnPercentage = Helper.getNumber(returnAmount / 100);
-		let calculatedTime = _durYears + (_durMonths / 12);
-		let finalValue = _futureAmountRequired / Math.pow((1 + calculatedReturnPercentage), calculatedTime);
-		return Helper.getReturnValue(finalValue);
-	},
-	// Calculates the future value of a periodic lump sum investment. The function takes the return rate, the required amount, the current investment amount, the duration in years and months, and the frequency of investment as inputs. It returns the future value of the investment.
-	getFutureAmountPeriodicLumpSumInvestment: function(returnAmount, amountRequired, amountToday, durationYears, durationMonths, frequency) {
-		let _futureAmountRequired = Helper.getNumber(amountRequired);
-		let _amountToday = Helper.getNumber(amountToday);
-		let _durYears = Helper.getNumber(durationYears);
-		let _durMonths = Helper.getNumber(durationMonths);
-		let _frequencySelection = frequencytoUpperCase();
-		let _frequency = 0;
-		let _duration = 0;
-		switch (_frequencySelection)
-		{
-			case Tags.frequencyAnnual:
-				_frequency = 1;
-				_duration = _durYears + (_durMonths / 12);
-				break;
-			case Tags.frequencyQuarterly:
-				_frequency = 4;
-				_duration = (_durYears * 4) + (_durMonths / 3);
-				break;
-			case Tags.frequencyMonthly:
-				_frequency = 12;
-				_duration = (_durYears * 12) + _durMonths;
-				break;
-			default:
-				_frequency = 0;
-				_duration = 0;
-				break;
-		}
-		let finalValue = Calculations.calculatePayment(Math.pow((1.0 + (returnAmount / 100.0)), (1.0 / _frequency)) - 1, _duration, _amountToday, -_futureAmountRequired, DueDate.BegOfPeriod);
-		return Helper.getReturnValue(finalValue);
-	},
-	// Calculates the future value of a required lump sum investment, assuming a leverage loan. The function takes the required amount, the duration in years, and the annual compound return rate as inputs. It returns the future value of the investment.
-	getFutureAmountLeveragedRequiredLumpSumLeveraged: function(requiredAmount, durationYears, annualCompoundReturn) {
-		let _amountRequired = Helper.getNumber(requiredAmount);
-		let _durYears = Helper.getNumber(durationYears);
-		let _annualCompoundReturn = Helper.getNumber(annualCompoundReturn);
-		let finalValue = NaN;
-		if (_durYears > 0 && _annualCompoundReturn > 0) {
-			finalValue = _amountRequired / (Math.pow(1 + (_annualCompoundReturn / 100.00), _durYears) - 1);
-		}
-		return Helper.getReturnValue(finalValue);
-	},
-	// Calculates the future value of an investment with leverage, given the required amount and the duration in years, and the annual compound return rate. The function returns the future value of the investment.
-	getFutureAmountLeveragedValueAmount: function(requiredAmount, durationYears, annualCompoundReturn) {
-		let _amountRequired = Helper.getNumber(requiredAmount);
-		let _durYears = Helper.getNumber(durationYears);
-		let _annualCompoundReturn = Helper.getNumber(annualCompoundReturn);
-		let finalValue = NaN;
-		if (_durYears > 0 && _annualCompoundReturn > 0) {
-			let requiredLumpSumValue = _amountRequired / (Math.pow(1 + (_annualCompoundReturn / 100.00), _durYears) - 1);
-			finalValue = requiredLumpSumValue * (Math.pow((1 + (_annualCompoundReturn / 100.00)), _durYears));
-		}
-		return Helper.getReturnValue(finalValue);
-	},
-	// Calculates the balance after loan repayment for an investment with leverage. The function takes the required amount, the duration in years, and the annual compound return rate as inputs. It returns the balance after loan repayment.
-	getFutureAmountLeveragedValueBalanceAfterLoan: function(requiredAmount, durationYears, annualCompoundReturn) {
-		let _amountRequired = Helper.getNumber(requiredAmount);
-		let _durYears = Helper.getNumber(durationYears);
-		let _annualCompoundReturn = Helper.getNumber(annualCompoundReturn);
-		let finalValue = NaN;
-		if (_durYears > 0 && _annualCompoundReturn > 0) {
-			let requiredLumpSumValue = _amountRequired / (Math.pow(1 + (_annualCompoundReturn / 100.00), _durYears) - 1);
-			let leveragedAmountValue = requiredLumpSumValue * (Math.pow((1 + (_annualCompoundReturn / 100.00)), _durYears));
-			finalValue = leveragedAmountValue - requiredLumpSumValue;
-		}
-		return Helper.getReturnValue(finalValue);
-	}
+  // Calculates the future value of a periodic investment. The function takes the return rate, the required amount, the duration in years and months, and the frequency of investment as inputs. It returns the future value of the investment.
+  getFutureAmountPeriodicInvestment: function (returnAmount, amountRequired, durationYears, durationMonths, frequency) {
+    let valueFutureAmountRequired = Helper.getNumber(amountRequired);
+    let valueDurationYears = Helper.getNumber(durationYears);
+    let valueDurationMonths = Helper.getNumber(durationMonths);
+    let valueFrequencySelection = frequencytoUpperCase();
+    let valueFrequency = 0;
+    let valueDurationation = 0;
+    switch (valueFrequencySelection) {
+      case Tags.frequencyAnnual:
+        valueFrequency = 1;
+        valueDurationation = valueDurationYears + (valueDurationMonths / 12);
+        break;
+      case Tags.frequencyQuarterly:
+        valueFrequency = 4;
+        valueDurationation = (valueDurationYears * 4) + (valueDurationMonths / 3);
+        break;
+      case Tags.frequencyMonthly:
+        valueFrequency = 12;
+        valueDurationation = (valueDurationYears * 12) + valueDurationMonths;
+        break;
+      default:
+        valueFrequency = 0;
+        valueDurationation = 0;
+        break;
+    }
+    let rate = Math.pow((1.0 + (returnAmount / 100)), (1.0 / valueFrequency)) - 1;
+    let duration = valueDurationation;
+    let finalValue = Calculations.calculatePayment(rate, duration, 0, -valueFutureAmountRequired, DueDate.BegOfPeriod);
+    return Helper.getReturnValue(finalValue);
+  },
+  // Calculates the future value of a lump sum investment. The function takes the return rate, the required amount, and the duration in years and months as inputs. It returns the future value of the investment.
+  getFutureAmountLumpSumInvestment: function (returnAmount, amountRequired, durationYears, durationMonths) {
+    let valueFutureAmountRequired = Helper.getNumber(amountRequired);
+    let valueDurationYears = Helper.getNumber(durationYears);
+    let valueDurationMonths = Helper.getNumber(durationMonths);
+    let calculatedReturnPercentage = Helper.getNumber(returnAmount / 100);
+    let calculatedTime = valueDurationYears + (valueDurationMonths / 12);
+    let finalValue = valueFutureAmountRequired / Math.pow((1 + calculatedReturnPercentage), calculatedTime);
+    return Helper.getReturnValue(finalValue);
+  },
+  // Calculates the future value of a periodic lump sum investment. The function takes the return rate, the required amount, the current investment amount, the duration in years and months, and the frequency of investment as inputs. It returns the future value of the investment.
+  getFutureAmountPeriodicLumpSumInvestment: function (returnAmount, amountRequired, amountToday, durationYears, durationMonths, frequency) {
+    let valueFutureAmountRequired = Helper.getNumber(amountRequired);
+    let valueAmountToday = Helper.getNumber(amountToday);
+    let valueDurationYears = Helper.getNumber(durationYears);
+    let valueDurationMonths = Helper.getNumber(durationMonths);
+    let valueFrequencySelection = frequencytoUpperCase();
+    let valueFrequency = 0;
+    let valueDurationation = 0;
+    switch (valueFrequencySelection) {
+      case Tags.frequencyAnnual:
+        valueFrequency = 1;
+        valueDurationation = valueDurationYears + (valueDurationMonths / 12);
+        break;
+      case Tags.frequencyQuarterly:
+        valueFrequency = 4;
+        valueDurationation = (valueDurationYears * 4) + (valueDurationMonths / 3);
+        break;
+      case Tags.frequencyMonthly:
+        valueFrequency = 12;
+        valueDurationation = (valueDurationYears * 12) + valueDurationMonths;
+        break;
+      default:
+        valueFrequency = 0;
+        valueDurationation = 0;
+        break;
+    }
+    let finalValue = Calculations.calculatePayment(Math.pow((1.0 + (returnAmount / 100.0)), (1.0 / valueFrequency)) - 1, valueDurationation, valueAmountToday, -valueFutureAmountRequired, DueDate.BegOfPeriod);
+    return Helper.getReturnValue(finalValue);
+  },
+  // Calculates the future value of a required lump sum investment, assuming a leverage loan. The function takes the required amount, the duration in years, and the annual compound return rate as inputs. It returns the future value of the investment.
+  getFutureAmountLeveragedRequiredLumpSumLeveraged: function (requiredAmount, durationYears, annualCompoundReturn) {
+    let valueAmountRequired = Helper.getNumber(requiredAmount);
+    let valueDurationYears = Helper.getNumber(durationYears);
+    let valueAnnualCompoundReturn = Helper.getNumber(annualCompoundReturn);
+    let finalValue = NaN;
+    if (valueDurationYears > 0 && valueAnnualCompoundReturn > 0) {
+      finalValue = valueAmountRequired / (Math.pow(1 + (valueAnnualCompoundReturn / 100.00), valueDurationYears) - 1);
+    }
+    return Helper.getReturnValue(finalValue);
+  },
+  // Calculates the future value of an investment with leverage, given the required amount and the duration in years, and the annual compound return rate. The function returns the future value of the investment.
+  getFutureAmountLeveragedValueAmount: function (requiredAmount, durationYears, annualCompoundReturn) {
+    let valueAmountRequired = Helper.getNumber(requiredAmount);
+    let valueDurationYears = Helper.getNumber(durationYears);
+    let valueAnnualCompoundReturn = Helper.getNumber(annualCompoundReturn);
+    let finalValue = NaN;
+    if (valueDurationYears > 0 && valueAnnualCompoundReturn > 0) {
+      let requiredLumpSumValue = valueAmountRequired / (Math.pow(1 + (valueAnnualCompoundReturn / 100.00), valueDurationYears) - 1);
+      finalValue = requiredLumpSumValue * (Math.pow((1 + (valueAnnualCompoundReturn / 100.00)), valueDurationYears));
+    }
+    return Helper.getReturnValue(finalValue);
+  },
+  // Calculates the balance after loan repayment for an investment with leverage. The function takes the required amount, the duration in years, and the annual compound return rate as inputs. It returns the balance after loan repayment.
+  getFutureAmountLeveragedValueBalanceAfterLoan: function (requiredAmount, durationYears, annualCompoundReturn) {
+    let valueAmountRequired = Helper.getNumber(requiredAmount);
+    let valueDurationYears = Helper.getNumber(durationYears);
+    let valueAnnualCompoundReturn = Helper.getNumber(annualCompoundReturn);
+    let finalValue = NaN;
+    if (valueDurationYears > 0 && valueAnnualCompoundReturn > 0) {
+      let requiredLumpSumValue = valueAmountRequired / (Math.pow(1 + (valueAnnualCompoundReturn / 100.00), valueDurationYears) - 1);
+      let leveragedAmountValue = requiredLumpSumValue * (Math.pow((1 + (valueAnnualCompoundReturn / 100.00)), valueDurationYears));
+      finalValue = leveragedAmountValue - requiredLumpSumValue;
+    }
+    return Helper.getReturnValue(finalValue);
+  }
 }
